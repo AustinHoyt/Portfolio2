9a539a77a38a16753144737ca4b804ca44aeffc5
Austin Hoyt <VanillaHoyt@gmail.com>
2017-08-02 02:04:51 +0000

    Changed mailer settings to ENV file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => ENV['MAILGUN_SMTP_SERVER'],
   :user_name            => ENV['MAILGUN_SMTP_LOGIN'],
   :password             => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain               => 'https://pure-depths-74182.herokuapp.com/',
+  :domain               => 'https://pacific-inlet-10678.herokuapp.com/',
   :authentication       => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
